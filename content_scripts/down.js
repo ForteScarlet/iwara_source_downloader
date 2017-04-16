0d0af8a5834150c6e2bbc85650ae317b35de36b1
mmduser1 <dummy>
2017-04-16 17:21:55 +0900

    念のためのチェックを追加

--- content_scripts/down.js
+++ content_scripts/down.js
@@ -13,11 +13,13 @@ function getInfo(){
   var title = info.getElementsByTagName('h1');
   var username = info.getElementsByTagName('a');
 
-  browser.runtime.sendMessage({
-    source_url: links[0].href,
-    title: convertSafeFileName(title[0].innerHTML),
-    username: convertSafeFileName(username[1].innerHTML)
-  });
+  if(links[0].href,indexOf("_Source") != -1){
+    browser.runtime.sendMessage({
+      source_url: links[0].href,
+      title: convertSafeFileName(title[0].innerHTML),
+      username: convertSafeFileName(username[1].innerHTML)
+    });
+  }
 }
 
 /*

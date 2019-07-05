a0ec8ee79085c536086a9599a1426570f3e66065
mmduser1 <mmd.watch@gmail.com>
2019-07-05 20:17:40 +0900

    ver 2.0.0

--- firefox/background.js
+++ firefox/background.js
@@ -1,69 +1,99 @@
-/*
-アイコンがクリックされたときにdown.jsへメッセージを送信する
-*/
-browser.pageAction.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {"download": "run"});
-});
+'use strict';
+{
+  /*
+  アイコンがクリックされたときにdown.jsへメッセージを送信する
+  */
+  chrome.pageAction.onClicked.addListener((tab) => {
+    chrome.tabs.sendMessage(tab.id, {"current_url": tab.url});
+  });
 
-/*
-down.jsから動画情報を受け取ってダウンロードを開始する
-*/
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    const setting = browser.storage.local.get();
-    setting.then((settings)=>{
-      var filename = request.username + ' - ' + request.title;
-      if (typeof settings.filename !== "undefined") {
-        if(settings.filename.indexOf("type1") != -1){
-          filename = request.username + ' - ' + request.title;// + '.mp4';
-        } else if(settings.filename.indexOf("type2") != -1) {
-          filename = '['+request.username+'] ' + request.title;
-        } else {
-          filename = request.title;
-        }
-      }
-      if(typeof settings.posted_date !== "undefined"){
-        if(settings.posted_date){
-          filename += "_" + request.posted_date;
-        }
-      }
-
+  let executeDownload = (info) => {
+    chrome.storage.local.get({
+      filename_definition: '?username? - ?title?'
+    },(settings)=>{
+      let filename = settings.filename_definition
+        .replace(/\?title\?/g, info.title)
+        .replace(/\?username\?/g, info.username)
+        .replace(/\?year\?/g, info.year)
+        .replace(/\?month\?/g, info.month)
+        .replace(/\?day\?/g, info.day)
+        .replace(/\?hour\?/g, info.hour)
+        .replace(/\?minute\?/g, info.minute)
+        .replace(/\?video-id\?/g, info.video_id)
+      filename = convertSafeFileName(filename)
       filename += '.mp4';
 
-      function onStartedDownload(id) {
-        //console.log("Started to download: "+id);
-      }
-
-      function onFailed(error) {
-        //console.log("Something stinks: "+error);
-      }
-
-      var startDownload = browser.downloads.download({
-        url : request.source_url,
+      chrome.downloads.download({
+        url : info.source_url,
         filename: filename,
         conflictAction : 'uniquify',
         saveAs : true
       });
-
-      startDownload.then(onStartedDownload, onFailed);
-    }, onError);
-});
-
-/* エラーログ */
-function onError(e) {
-  console.error(e);
-}
-
-/*
-タブのURLにiwara.tv/videos/があればアイコンを表示
-*/
-function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf("iwara.tv/videos/") > -1) {
-    chrome.pageAction.show(tabId);
+    });
   }
-}
 
-/*
-タブの切り替えを捕捉
-*/
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+  /*
+  down.jsから動画情報を受け取ってダウンロードを開始する
+  */
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    executeDownload(request);
+  });
+
+  /*
+  タブのURLにiwara.tv/videos/があればアイコンを表示
+  */
+  let checkForValidUrl = (tabId, changeInfo, tab) => {
+    if (tab.url.indexOf("iwara.tv/videos/") > -1) {
+      chrome.pageAction.show(tabId);
+    }
+  }
+
+  /*
+  タブの切り替えを捕捉
+  */
+  chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+  /**
+   * 使用できない文字を全角に置き換え
+   * ¥　/　:　*　?　"　<　>　| tab
+   *
+   * @param {String} str 変換したい文字列
+   */
+  let convertSafeFileName = (titleOrUsername) => {
+    return unEscapeHTML(titleOrUsername)
+      .replace(/\\/g,'￥')
+      .replace(/\//g,'／')
+      .replace(/:/g,'：')
+      .replace(/\*/g,'＊')
+      .replace(/\?/g,'？')
+      .replace(/"/g,'”')
+      .replace(/</g,'＜')
+      .replace(/>/g,'＞')
+      .replace(/\|/g,'｜')
+      .replace(/\t/g, '　');
+  }
+
+  /**
+   * HTMLアンエスケープ
+   *
+   * @param {String} str 変換したい文字列
+   */
+  let unEscapeHTML = (str) => {
+    return str
+      .replace(/(&lt;)/g, '<')
+      .replace(/(&gt;)/g, '>')
+      .replace(/(&quot;)/g, '"')
+      .replace(/(&#39;)/g, "'")
+      .replace(/(&amp;)/g, '&');
+  };
+
+  /**
+   * アップデート時にメッセージを表示
+   */
+  chrome.runtime.onInstalled.addListener((OnInstalledReason) => {
+    console.info(Notification.permission );
+    if (OnInstalledReason.reason === "update") {
+      browser.runtime.openOptionsPage();
+    }
+  });
+}
--- firefox/content_scripts/down.js
+++ firefox/content_scripts/down.js
@@ -1,75 +1,51 @@
-/*
-動画の情報を取得してbackgroundへ投げる
-取得する情報：
-  ・SourceのURL
-  ・タイトル
-  ・アップロードしたユーザーの名前
-*/
-function getInfo(){
-  var button = document.getElementById("download-button");
-  button.click();
-  var links = document.getElementById("download-options").getElementsByTagName('a');
-  var info = document.getElementsByClassName('node-info')[0];
-  var title = info.getElementsByTagName('h1');
-  var username = info.getElementsByTagName('img');
-  var html_lang = document.getElementsByTagName('html')[0].lang;
-  if(html_lang === 'ja') {
-    username = username[0].title.replace("ユーザー ","").replace(" の写真","");
-  } else if(html_lang === 'en') {
-    username = username[0].title.replace(/(&#39;)/, "'").replace("'s picture","");
-  } else if(html_lang === 'zh-hans') {
-    username = username[0].title.replace("的头像", "");
-  } else {
-    username = username[0].title.replace("Bild des Benutzers ", "");
-  }
-  var posted_date = info.textContent.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/);
-  if(posted_date !== null){
-    posted_date = posted_date[0].replace(/-/g, "").replace(/:/g, "").replace(" ","");
+'use strict';
+{
+  /**
+   * 動画の情報を取得してbackgroundへ投げる
+   */
+  let getInfo = (request, sender, sendResponse) => {
+    let button = document.getElementById("download-button");
+    button.click();
+    let links = document.getElementById("download-options").getElementsByTagName('a');
+    let info = document.getElementsByClassName('node-info')[0];
+    let title = info.getElementsByTagName('h1');
+    let username = info.getElementsByTagName('img');
+    let html_lang = document.getElementsByTagName('html')[0].lang;
+    if(html_lang === 'ja') {
+      username = username[0].title.replace("ユーザー ","").replace(" の写真","");
+    } else if(html_lang === 'en') {
+      username = username[0].title.replace(/(&#39;)/, "'").replace("'s picture","");
+    } else if(html_lang === 'zh-hans') {
+      username = username[0].title.replace("的头像", "");
+    } else {
+      username = username[0].title.replace("Bild des Benutzers ", "");
+    }
+    let posted_date = info.textContent.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/);
+    let year = posted_date[1];
+    let month = posted_date[2];
+    let day = posted_date[3];
+    let hour = posted_date[4];
+    let minute = posted_date[5];
+
+    let video_id = new URL(request.current_url).pathname.split("/").pop();
+
+    if(links[0].href.indexOf("_Source") != -1){
+      chrome.runtime.sendMessage({
+        source_url: links[0].href,
+        title: title[0].innerText,
+        username: username,
+        year: year,
+        month: month,
+        day: day,
+        hour: hour,
+        minute: minute,
+        video_id: video_id
+      });
+    }
   }
 
-  if(links[0].href.indexOf("_Source") != -1){
-    browser.runtime.sendMessage({
-      source_url: links[0].href,
-      title: convertSafeFileName(title[0].innerHTML),
-      username: convertSafeFileName(username),
-      posted_date: posted_date
-    });
-  }
+  /**
+   * backgroundからのメッセージを受信したらgetInfoを実行
+   */
+  chrome.runtime.onMessage.addListener(getInfo);
 }
-
-/*
-backgroundからのメッセージを受信したらgetInfoを実行
-*/
-chrome.runtime.onMessage.addListener(getInfo);
-
-/*
-使用できない文字を全角に置き換え
-¥　/　:　*　?　"　<　>　| tab
-*/
-function convertSafeFileName(titleOrUsername){
-  return unEscapeHTML(titleOrUsername)
-    .replace(/\\/g,'￥')
-    .replace(/\//g,'／')
-    .replace(/:/g,'：')
-    .replace(/\*/g,'＊')
-    .replace(/\?/g,'？')
-    .replace(/"/g,'”')
-    .replace(/</g,'＜')
-    .replace(/>/g,'＞')
-    .replace(/\|/g,'｜')
-    .replace(/\t/g, '　');
-}
-
-/**
- * HTMLアンエスケープ
- *
- * @param {String} str 変換したい文字列
- */
-var unEscapeHTML = function (str) {
-    return str
-      .replace(/(&lt;)/g, '<')
-      .replace(/(&gt;)/g, '>')
-      .replace(/(&quot;)/g, '"')
-      .replace(/(&#39;)/g, "'")
-      .replace(/(&amp;)/g, '&');
-};

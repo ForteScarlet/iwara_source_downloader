0dbab187909c0b4f8b6c46039634e8c093a3ae81
mmduser1 <mmd.watch@gmail.com>
2019-07-05 11:54:07 +0900

    start ver 2.0.0

--- chrome/background.js
+++ chrome/background.js
@@ -1,69 +1,56 @@
-/*
-アイコンがクリックされたときにdown.jsへメッセージを送信する
-*/
-chrome.pageAction.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {"download": "run"});
-});
+'use strict';
+{
+  /*
+  アイコンがクリックされたときにdown.jsへメッセージを送信する
+  */
+  chrome.pageAction.onClicked.addListener((tab) => {
+    chrome.tabs.sendMessage(tab.id, {"download": "run"});
+  });
 
-/*
-down.jsから動画情報を受け取ってダウンロードを開始する
-*/
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-
-    chrome.storage.local.get(["filename","posted_date"],((settings)=>{
-
-      var filename = request.username + ' - ' + request.title;
-      if (typeof settings.filename !== "undefined") {
-        if(settings.filename.indexOf("type1") != -1){
-          filename = request.username + ' - ' + request.title;
-        } else if(settings.filename.indexOf("type2") != -1) {
-          filename = '['+request.username+'] ' + request.title;
-        } else {
-          filename = request.title;
+  /*
+  down.jsから動画情報を受け取ってダウンロードを開始する
+  */
+  chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+      chrome.storage.local.get(["filename","posted_date"],((settings)=>{
+        var filename = request.username + ' - ' + request.title;
+        if (typeof settings.filename !== "undefined") {
+          if(settings.filename.indexOf("type1") != -1){
+            filename = request.username + ' - ' + request.title;
+          } else if(settings.filename.indexOf("type2") != -1) {
+            filename = '['+request.username+'] ' + request.title;
+          } else {
+            filename = request.title;
+          }
         }
-      }
-      if(typeof settings.posted_date !== "undefined"){
-        if(settings.posted_date){
-          filename += "_" + request.posted_date;
+        if(typeof settings.posted_date !== "undefined"){
+          if(settings.posted_date){
+            filename += "_" + request.posted_date;
+          }
         }
-      }
-      filename += '.mp4';
+        filename += '.mp4';
 
-      function onStartedDownload(id) {
-        //console.log("Started to download: "+id);
-      }
+        let startDownload = chrome.downloads.download({
+          url : request.source_url,
+          filename: filename,
+          conflictAction : 'prompt',
+          saveAs : true
+        });
 
-      function onFailed(error) {
-        //console.log("Something stinks: "+error);
-      }
+      }));
+  });
 
-      var startDownload = chrome.downloads.download({
-        url : request.source_url,
-        filename: filename,
-        conflictAction : 'prompt',
-        saveAs : true
-      });
-
-      //startDownload.then(onStartedDownload, onFailed);
-    }));
-});
-
-/* エラーログ */
-function onError(e) {
-  console.error(e);
-}
-
-/*
-タブのURLにiwara.tv/videos/があればアイコンを表示
-*/
-function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf("iwara.tv/videos/") > -1) {
-    chrome.pageAction.show(tabId);
+  /*
+  タブのURLにiwara.tv/videos/があればアイコンを表示
+  */
+  let checkForValidUrl = (tabId, changeInfo, tab) => {
+    if (tab.url.indexOf("iwara.tv/videos/") > -1) {
+      chrome.pageAction.show(tabId);
+    }
   }
-}
 
-/*
-タブの切り替えを捕捉
-*/
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+  /*
+  タブの切り替えを捕捉
+  */
+  chrome.tabs.onUpdated.addListener(checkForValidUrl);
+}

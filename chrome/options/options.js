b785fc313a76e664452acb92ba5db9822eb73e75
mmduser1 <mmd.watch@gmail.com>
2021-11-23 15:03:49 +0900

    update option

--- chrome/options/options.js
+++ chrome/options/options.js
@@ -5,9 +5,19 @@ function storeSettings() {
 
   let auto_like = document.getElementById("auto_like");
   let filename_definition = document.getElementById("filename_definition");
+  let download_modes = document.getElementsByName("download_mode");
+  let download_mode = "1";
+
+  for (let i = 0; i < download_modes.length; i++) {
+    if (download_modes[i].checked) {
+      download_mode = download_modes[i].value;
+      break;
+    }
+  }
 
   chrome.storage.local.set({
     auto_like: auto_like.checked,
+    download_mode: download_mode,
     filename_definition: filename_definition.value
   }, updateStatus);
 
@@ -19,6 +29,12 @@ function storeSettings() {
 */
 function updateUI(restoredSettings) {
   document.getElementById('auto_like').checked = restoredSettings.auto_like;
+
+  if (typeof restoredSettings.download_mode !== "undefined") {
+    document.getElementById('download_mode'+restoredSettings.download_mode).checked = true
+  }else{
+    document.getElementById('download_mode1').checked = true
+  }
 
   let filename_definition = document.getElementById("filename_definition");
 
